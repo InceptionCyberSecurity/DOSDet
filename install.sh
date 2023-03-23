26058fe27a1e1d50c80da8934e64fe8eba37e86e
Arcadeus OPS <info@arcadeusops.com>
2023-03-23 11:09:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install tools
 echo " "
 echo " Make sure you have setup your outgoing SMTP mail server as per the instructions on screen. "
-echo " You need to specify your Gmail credentials to allow SMTP traffic."
+echo " You need to specify your email server logon credentials to allow SMTP traffic."
 echo " "
 sudo apt update
 sudo apt-get install net-tools nload mailutils ssmtp epel-release -y

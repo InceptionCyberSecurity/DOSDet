1b5e4094476cc51d76284afebb7cbe8ac2370deb
Arcadeus OPS <info@arcadeusops.com>
2023-03-23 11:09:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Install tools
-sudo apt update
-sudo apt-get install net-tools nload mailutils ssmtp epel-release -y
-# nano /etc/ssmtp/ssmtp.conf
-# change email addresses to your Gmail account
 echo " "
 echo " Make sure you have setup your outgoing SMTP mail server as per the instructions on screen. "
 echo " You need to specify your Gmail credentials to allow SMTP traffic."
 echo " "
+sudo apt update
+sudo apt-get install net-tools nload mailutils ssmtp epel-release -y
+
+# nano /etc/ssmtp/ssmtp.conf
+# change email addresses to your Gmail account

b34769184ab4c0a8f5130705d443f4b3f605ac23
Arcadeus OPS <info@arcadeusops.com>
2023-03-24 08:33:09 +0800

    Update dosde.sh

--- dosde.sh
+++ dosde.sh
@@ -4,14 +4,14 @@
 echo "  "
 echo " Confirm you have setup your outgoing mail server as per the instructions during install. "
 echo " install.sh will install and setup a SMTP server on this local machine. "
-echo " You need to specify your Gmail credentials to allow SMTP traffic, for example."
+echo " You need to specify your Gmail credentials to allow SMTP traffic. "
 echo " "
 echo " RUN THIS netstat COMMAND FIRST TO GET ATTACKING IP ADDRESS and look in netst.txt .... "
 echo " netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r > netst.txt "
 echo " "
 read -p " What email do you want the results to be sent to ? " umail
 read -p " Look in netst.txt. What IP address would you like blocked? Enter 0.0.0.0 if no IP address known " ipban
-
+echo " "
 # Check server load. If number is high, investigate.
 grep processor /proc/cpuinfo | wc -l > load.txt
 sed -i -e '1iServer Load\' load.txt
@@ -28,7 +28,6 @@ sed -i -e '2i*************************\' netst.txt
 cat load.txt nload.txt netst.txt | sort > dos.txt
 sed -i -e '1iIs a DOS attack happening right now?\' dos.txt
 sed -i -e '2i*********************************************************\' dos.txt
-
 # ban attacking IP
 route add $ipban reject
 route -n |grep $ipban
@@ -39,7 +38,6 @@ service iptables save
 # Now kill all httpd connections and restart httpd services
 killall -KILL httpd
 service httpd startssl
-
 # mail
 mail -s "Initial and Quick Malware Report" $umail -a dos.txt
 echo " Report sent to $umail. "
